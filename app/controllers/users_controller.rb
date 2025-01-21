d21c54cc09742641a39a450b1cc4a79f3df79700
Luke Webb <lukewebb2000@gmail.com>
2025-01-22 12:31:45 +1300

    Add route and linked up the form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def create
+    # create and return a new user object and save them to the db.
+  end
+
+  def new
+    # create a new user object.
+  end
+end
