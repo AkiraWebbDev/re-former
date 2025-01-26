a0cbc9f3fd559085387dec2aefbb6a3cd857a610
Luke Webb <lukewebb2000@gmail.com>
2025-01-26 15:52:34 +1300

    Added index to assist in debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def create
-    # create and return a new user object and save them to the db.
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def new
-    # create a new user object.
+    @user = User.new()
   end
+
+  private
+    def user_params
+      params.expect(user: [ :username, :email, :password ])
+    end
 end
