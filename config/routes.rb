0de51d3ec18f4d9cd401ce08a76515b18d0c0d3c
Luke Webb <lukewebb2000@gmail.com>
2025-01-26 16:26:10 +1300

    Allow for editing of existing users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users, only: [ :new, :create, :update ]
+  resource :users, only: [ :new, :create, :edit ]
 
   root "users#index"
 
